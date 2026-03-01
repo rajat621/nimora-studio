f0222a57f275c86e6ce2e6989a84a0f45c80239f
Rajat Raj <rajatraj9470@gmail.com>
2026-03-01 07:04:44 +0530

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -27,3 +27,41 @@
 
 //   return Response.json({ success: true });
 // }
+
+
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "rajatraj9492@gmail.com", // put your gmail here
+        pass: "safisngt@r74837", // put your gmail app password here
+      },
+    });
+
+    await transporter.sendMail({
+      from: "yourgmail@gmail.com",
+      to: "yourgmail@gmail.com",
+      subject: "New Contact Form Submission",
+      html: `
+        <h3>New Submission</h3>
+        <p><b>Name:</b> ${formData.get("name")}</p>
+        <p><b>Email:</b> ${formData.get("email")}</p>
+        <p><b>Phone:</b> ${formData.get("phone")}</p>
+        <p><b>Company:</b> ${formData.get("company")}</p>
+        <p><b>Message:</b> ${formData.get("message")}</p>
+      `,
+    });
+
+    return Response.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ success: false });
+  }
+}
